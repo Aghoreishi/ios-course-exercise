be70c6d9237088fc5e55325f6ee5efe100a83fb0
Amir Ghoreishi <Amir@Seyeds-iMac.local>
2016-01-31 19:31:03 -0600

    added files for Gold or Money

---
+++ Exercise 15/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Exercise 15
+//
+//  Created by Amir Ghoreishi on 1/30/16.
+//  Copyright © 2016 Amir Ghoreishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var potOfGold: UIImageView!
+    @IBOutlet weak var bagOfMoney: UIImageView!
+    @IBOutlet weak var takeGold: UIButton!
+    @IBOutlet weak var takeMoney: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func takeGold(sender: AnyObject) {
+        bagOfMoney.hidden = false
+        potOfGold.hidden = true
+        
+    }
+
+    @IBAction func takeMoney(sender: AnyObject) {
+        potOfGold.hidden = false
+        bagOfMoney.hidden = true
+        
+    }
+
+}
+
